35385017fd5ca601e1ad1c8a996a21eaa03aea5d
Nuuvy7 <kenjundi71@gmail.com>
2025-11-20 13:53:16 +0700

    logiasgdnd

--- login-provire/database.php
+++ login-provire/database.php
@@ -4,5 +4,12 @@ $username = "root";
 $password = "";
 $database_name = "login_sc";
 
-$db = mysql_connect($hostname, $username ,$password, $database_name)
+$db = mysql_connect($hostname, $username ,$password, $database_name);
+
+if ($db -> Connect_error) {
+    echo "koneksi terputus";
+    die ("mati!");
+}
+echo "koneksi berhasil";
+
 ?>
