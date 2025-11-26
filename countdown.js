f9805fb20e063b17cd5060f53662e2c5b89621fc
Nuuvy7 <kenjundi71@gmail.com>
2025-11-26 20:16:44 +0700

    Terimakasih Arassy

---
+++ countdown.js
@@ -0,0 +1,34 @@
+const targetDate = new Date("2025-12-31T23:59:59").getTime();
+const countdownEl = document.querySelector("[countdown]");
+
+    function updateCountdown() {
+        const now = new Date().getTime();
+        const diff = targetDate - now;
+
+        if (diff <= 0) {
+            countdownEl.innerHTML = "<span style='font-weight:600;'>Waktu Habis!</span>";
+            return;
+        }
+
+        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+        countdownEl.innerHTML = `
+            <div style="
+                font-size: 16px;
+                margin-top: 10px;
+                font-weight: 600;
+                letter-spacing: 1px;
+            ">
+                ${days} Days 
+                ${hours} Hours 
+                ${minutes} Minutes 
+                ${seconds} Seconds
+            </div>
+        `;
+    }
+
+    updateCountdown();
+    setInterval(updateCountdown, 1000);
