86aab74e1687dfd9467d01af13519d6d36bd9350
Nuuvy7 <kenjundi71@gmail.com>
2025-11-27 22:11:50 +0700

    nav

--- navbar.js
+++ navbar.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (window.scrollY >= window.innerHeight) {
             // Di section 2+, height naik ke nilai penuh (misalnya 50px), opacity 1, background solid
             navbar.style.height = 'auto'; // Ganti dengan tinggi navbar yang kamu inginkan, e.g., 'auto' jika fleksibel
-            navbar.style.opacity = '1';
+            navbar.style.opacity = '0.9';
             navbar.style.backgroundColor = '#000000ff'; // Ganti warna solid
             navbar.style.padding = '15px 25px'; 
         } else {
