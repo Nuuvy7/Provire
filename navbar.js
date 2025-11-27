c322117fccfe47ddaab2ebc0da3f08b53cf868d7
Nuuvy7 <kenjundi71@gmail.com>
2025-11-27 23:11:09 +0700

    nav222

--- navbar.js
+++ navbar.js
@@ -3,18 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function updateNavbar() {
         if (window.scrollY >= window.innerHeight) {
-            // Di section 2+, height naik ke nilai penuh (misalnya 50px), opacity 1, background solid
-            navbar.style.height = 'auto'; // Ganti dengan tinggi navbar yang kamu inginkan, e.g., 'auto' jika fleksibel
+            //section 2+
+            navbar.style.height = 'auto'; 
             navbar.style.opacity = '0.9';
-            navbar.style.backgroundColor = '#000000ff'; // Ganti warna solid
-            navbar.style.padding = '15px 15px';
+            navbar.style.backgroundColor = '#184188';
+            navbar.style.padding = '5px 15px';
             navbar.style.top = '0';
         } else {
-            // Di section 1, height 0, opacity 0, background transparan
+            //section 1
             navbar.style.height = '0';
             navbar.style.opacity = '0';
             navbar.style.backgroundColor = 'transparent';
-            navbar.style.padding = '0px 0px'; 
+            navbar.style.padding = '0 0'; 
         }
     }
 
